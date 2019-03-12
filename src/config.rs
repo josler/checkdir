5073c227241664f1544aaa2ed290d79e826e08f2
josler <j.osler@gmail.com>
2019-03-12 08:35:58 -0400

    read config

---
+++ src/config.rs
@@ -0,0 +1,30 @@
+use dirs;
+use serde_derive::*;
+use serde_json;
+use std::fs;
+
+use std::path::PathBuf;
+
+#[derive(Deserialize, Serialize, Debug)]
+pub struct Config {
+    #[serde(default)]
+    pub ignore_dirs: Vec<String>,
+    #[serde(default)]
+    pub ignore_paths: Vec<String>,
+}
+
+impl Config {
+    pub fn new(path: &PathBuf) -> Self {
+        let path = path.join(".hammersyncconfig");
+        match fs::read_to_string(&path) {
+            Ok(d) => serde_json::from_str(&d).unwrap(),
+            Err(_) => {
+                let c = Config {
+                    ignore_dirs: vec![],
+                    ignore_paths: vec![],
+                };
+                c
+            }
+        }
+    }
+}
